934d0eac5541e4b33cc097f1f8ccb679874354c2
Libing Chen <libing.chen@gmail.com>
2025-08-09 21:33:48 +0800

    chore: DotenvxBundle

--- src/main/kotlin/com/github/linuxchina/dotenvx/DotenvxBundle.kt
+++ src/main/kotlin/com/github/linuxchina/dotenvx/DotenvxBundle.kt
@@ -1,13 +1,13 @@
-package com.github.linuxchina.dotenvxjetbrainsplugin
+package com.github.linuxchina.dotenvx
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
 
 @NonNls
-private const val BUNDLE = "messages.MyBundle"
+private const val BUNDLE = "messages.DotenvxBundle"
 
-object MyBundle : DynamicBundle(BUNDLE) {
+object DotenvxBundle : DynamicBundle(BUNDLE) {
 
     @JvmStatic
     fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
