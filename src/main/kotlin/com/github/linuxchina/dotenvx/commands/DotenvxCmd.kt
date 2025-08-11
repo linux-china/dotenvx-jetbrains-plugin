75f2d61f7338703130ea3c67ed453792ffbe3efa
Libing Chen <libing.chen@gmail.com>
2025-08-11 22:47:34 +0800

    feat: env file name could be absolute path

--- src/main/kotlin/com/github/linuxchina/dotenvx/commands/DotenvxCmd.kt
+++ src/main/kotlin/com/github/linuxchina/dotenvx/commands/DotenvxCmd.kt
@@ -1,15 +1,26 @@
 package com.github.linuxchina.dotenvx.commands
 
 import io.github.cdimascio.dotenv.DotenvxBuilder
+import java.io.File
 import java.nio.file.Path
 
-class DotenvxCmd(private val workDir: String, private val envFile: String?) {
+class DotenvxCmd(private val workDir: String, private val envFileName: String?) {
 
     fun importEnv(): Map<String, String> {
         val dotenvxVariables: MutableMap<String, String> = mutableMapOf()
-        if (Path.of(workDir, envFile ?: ".env").toFile().exists()) {
+        val envFile = File(envFileName ?: ".env")
+        if (envFile.isAbsolute) {
             try {
-                val dotenv = DotenvxBuilder().directory(workDir).filename(envFile).load()
+                val dotenv = DotenvxBuilder().directory(envFile.parentFile.canonicalPath).filename(envFile.name).load()
+                dotenv.entries().forEach { entry ->
+                    dotenvxVariables[entry.key] = entry.value
+                }
+            } catch (_: Exception) {
+                // Handle exception if needed, e.g., log it
+            }
+        } else if (Path.of(workDir, envFileName ?: ".env").toFile().exists()) {
+            try {
+                val dotenv = DotenvxBuilder().directory(workDir).filename(envFileName).load()
                 dotenv.entries().forEach { entry ->
                     dotenvxVariables[entry.key] = entry.value
                 }
