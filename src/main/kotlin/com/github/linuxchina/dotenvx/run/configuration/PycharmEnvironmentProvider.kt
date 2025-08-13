6a35334963589f6466ee56e149d895af0077f7f5
Libing Chen <libing.chen@gmail.com>
2025-08-13 09:52:13 +0800

    chore: extends PythonCommandLineEnvironmentProvider

--- src/main/kotlin/com/github/linuxchina/dotenvx/run/configuration/PycharmEnvironmentProvider.kt
+++ src/main/kotlin/com/github/linuxchina/dotenvx/run/configuration/PycharmEnvironmentProvider.kt
@@ -2,20 +2,21 @@ package com.github.linuxchina.dotenvx.run.configuration
 
 import com.github.linuxchina.dotenvx.settings.DotenvxSettings
 import com.github.linuxchina.dotenvx.settings.ui.RunConfigSettingsEditor
+import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.projectRoots.SdkAdditionalData
 import com.jetbrains.python.run.AbstractPythonRunConfiguration
-import com.jetbrains.python.run.PythonExecution
+import com.jetbrains.python.run.PythonCommandLineEnvironmentProvider
 import com.jetbrains.python.run.PythonRunParams
-import com.jetbrains.python.run.target.HelpersAwareTargetEnvironmentRequest
-import com.jetbrains.python.run.target.PythonCommandLineTargetEnvironmentProvider
 import java.util.function.Consumer
 import java.util.stream.Stream
 
-class PycharmEnvironmentProvider : PythonCommandLineTargetEnvironmentProvider {
-    override fun extendTargetEnvironment(
+class PycharmEnvironmentProvider : PythonCommandLineEnvironmentProvider {   //PythonCommandLineTargetEnvironmentProvider
+
+    override fun extendEnvironment(
         project: Project,
-        helpersAwareTargetEnvironmentRequest: HelpersAwareTargetEnvironmentRequest,
-        pythonExecution: PythonExecution,
+        sdkAdditionalData: SdkAdditionalData,
+        generalCommandLine: GeneralCommandLine,
         pythonRunParams: PythonRunParams
     ) {
         if (pythonRunParams !is AbstractPythonRunConfiguration<*>) {
@@ -26,19 +27,13 @@ class PycharmEnvironmentProvider : PythonCommandLineTargetEnvironmentProvider {
         val dotenvxVariables: MutableMap<String, String> =
             RunConfigSettingsEditor.collectEnv(dotenvxSettings, pythonRunParams.workingDirectory)
         val runConfigurationVariables = pythonRunParams.envs
-
         Stream.concat(dotenvxVariables.entries.stream(), runConfigurationVariables.entries.stream())
-            .forEach(addEnvironmentVariableToPythonExecution(pythonExecution))
+            .forEach(addEnvironmentVariableToPythonExecution(generalCommandLine))
     }
 
-    companion object {
-        private fun addEnvironmentVariableToPythonExecution(pythonExecution: PythonExecution): Consumer<MutableMap.MutableEntry<String, String>> {
-            return Consumer { entry: MutableMap.MutableEntry<String, String> ->
-                pythonExecution.addEnvironmentVariable(
-                    entry.key,
-                    entry.value
-                )
-            }
+    private fun addEnvironmentVariableToPythonExecution(generalCommandLine: GeneralCommandLine): Consumer<MutableMap.MutableEntry<String, String>> {
+        return Consumer { entry: MutableMap.MutableEntry<String, String> ->
+            generalCommandLine.environment[entry.key] = entry.value
         }
     }
 }
