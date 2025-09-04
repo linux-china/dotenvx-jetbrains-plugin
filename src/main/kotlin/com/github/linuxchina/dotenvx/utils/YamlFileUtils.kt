332a3c449c8e7b435c16d81a32e51c1ee2f3517b
Libing Chen <libing.chen@gmail.com>
2025-09-04 18:39:30 +0800

    app: YamlFileUtils

---
+++ src/main/kotlin/com/github/linuxchina/dotenvx/utils/YamlFileUtils.kt
@@ -0,0 +1,27 @@
+package com.github.linuxchina.dotenvx.utils
+
+import com.intellij.openapi.editor.Editor
+import com.intellij.psi.PsiFile
+
+object YamlFileUtils {
+
+    fun isYamlOrToml(psiFile: PsiFile): Boolean {
+        val name = psiFile.name
+        return name.endsWith(".yaml") || name.endsWith(".yml") || name.endsWith(".toml")
+    }
+
+    fun getKeyNameOnLine(editor: Editor): String {
+        val document = editor.document
+        val caretModel = editor.caretModel
+        val offset = caretModel.offset
+        val lineNumber = document.getLineNumber(offset)
+        val lineStartOffset = document.getLineStartOffset(lineNumber)
+        val lineEndOffset = document.getLineEndOffset(lineNumber)
+        val keyName = document.getText(com.intellij.openapi.util.TextRange(lineStartOffset, lineEndOffset)).trim()
+        if (keyName.contains(':')) {
+            return keyName.substringBefore(':').trim()
+        } else {
+            return keyName
+        }
+    }
+}
