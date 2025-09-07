cc2ec7371172b8d8c12a6d176b0a1c1adb8f08b5
Libing Chen <libing.chen@gmail.com>
2025-09-07 12:16:24 +0800

    refactor: rename

--- src/main/kotlin/com/github/linuxchina/dotenvx/processor/PasteEncryptPreProcessor.kt
+++ src/main/kotlin/com/github/linuxchina/dotenvx/processor/PasteEncryptPreProcessor.kt
@@ -10,7 +10,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
 
 
-class PasteEncryptProcessor : CopyPastePreProcessor {
+class PasteEncryptPreProcessor : CopyPastePreProcessor {
 
     override fun preprocessOnCopy(
         file: PsiFile?,
