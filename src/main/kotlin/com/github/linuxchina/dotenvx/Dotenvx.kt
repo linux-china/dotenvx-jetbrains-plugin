693a5737b3f464779f4dcde38e119f6a1e4cca44
Libing Chen <libing.chen@gmail.com>
2025-09-09 22:24:55 +0800

    app: Dotenvx

---
+++ src/main/kotlin/com/github/linuxchina/dotenvx/Dotenvx.kt
@@ -0,0 +1,7 @@
+package com.github.linuxchina.dotenvx
+
+import com.github.linuxchina.dotenvx.intentions.EncryptEnvValueIntention
+import com.intellij.openapi.util.IconLoader
+
+val LOCKER_ICON = IconLoader.getIcon("icons/locker.svg", EncryptEnvValueIntention::class.java)
+val VARIABLE_ICON = IconLoader.getIcon("icons/key-value.svg", EncryptEnvValueIntention::class.java)
