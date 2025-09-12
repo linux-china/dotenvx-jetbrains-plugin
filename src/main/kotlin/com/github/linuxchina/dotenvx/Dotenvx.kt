a3a6b111c6ed53b21d492960e1adf938aaf7c9c9
Libing Chen <libing.chen@gmail.com>
2025-09-12 19:44:03 +0800

    chore: add KEY_ICON

--- src/main/kotlin/com/github/linuxchina/dotenvx/Dotenvx.kt
+++ src/main/kotlin/com/github/linuxchina/dotenvx/Dotenvx.kt
@@ -5,3 +5,4 @@ import com.intellij.openapi.util.IconLoader
 
 val LOCKER_ICON = IconLoader.getIcon("icons/locker.svg", EncryptEnvValueIntention::class.java)
 val VARIABLE_ICON = IconLoader.getIcon("icons/key-value.svg", EncryptEnvValueIntention::class.java)
+val KEY_ICON = IconLoader.getIcon("icons/key.svg", EncryptEnvValueIntention::class.java)
