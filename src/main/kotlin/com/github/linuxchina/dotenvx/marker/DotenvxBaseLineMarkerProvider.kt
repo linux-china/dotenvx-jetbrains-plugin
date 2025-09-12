67c518fa97f58bf916db8019fe36dd005031653d
Libing Chen <libing.chen@gmail.com>
2025-09-12 19:43:52 +0800

    app: DotenvxBaseLineMarkerProvider

---
+++ src/main/kotlin/com/github/linuxchina/dotenvx/marker/DotenvxBaseLineMarkerProvider.kt
@@ -0,0 +1,41 @@
+package com.github.linuxchina.dotenvx.marker
+
+import com.github.linuxchina.dotenvx.KEY_ICON
+import com.github.linuxchina.dotenvx.VARIABLE_ICON
+import com.intellij.codeInsight.daemon.LineMarkerInfo
+import com.intellij.codeInsight.daemon.LineMarkerProviderDescriptor
+import com.intellij.openapi.editor.markup.GutterIconRenderer
+import com.intellij.psi.PsiElement
+
+abstract class DotenvxBaseLineMarkerProvider : LineMarkerProviderDescriptor() {
+
+    fun lineMarkerForVariable(psiElement: PsiElement): LineMarkerInfo<PsiElement> {
+        return LineMarkerInfo(
+            psiElement,
+            psiElement.textRange,
+            VARIABLE_ICON,
+            { _: PsiElement? ->
+                "Dotenvx encrypted variable"
+            }, null,
+            GutterIconRenderer.Alignment.CENTER,
+            {
+                "Dotenvx encrypted variable"
+            }
+        )
+    }
+
+    fun lineMarkerForPublicKey(psiElement: PsiElement): LineMarkerInfo<PsiElement> {
+        return LineMarkerInfo(
+            psiElement,
+            psiElement.textRange,
+            KEY_ICON,
+            { _: PsiElement? ->
+                "Dotenvx public key"
+            }, null,
+            GutterIconRenderer.Alignment.CENTER,
+            {
+                "Dotenvx public key"
+            }
+        )
+    }
+}
