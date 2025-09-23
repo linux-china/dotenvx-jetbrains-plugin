fb01377768a52b084c82ed048542124848990771
Libing Chen <libing.chen@gmail.com>
2025-09-23 23:13:03 +0800

    chore: JsonFileLineMarkerProvider

---
+++ src/main/kotlin/com/github/linuxchina/dotenvx/marker/JsonFileLineMarkerProvider.kt
@@ -0,0 +1,25 @@
+package com.github.linuxchina.dotenvx.marker
+
+import com.intellij.codeInsight.daemon.GutterName
+import com.intellij.codeInsight.daemon.LineMarkerInfo
+import com.intellij.json.psi.JsonLiteral
+import com.intellij.psi.PsiElement
+
+class JsonFileLineMarkerProvider : DotenvxBaseLineMarkerProvider() {
+
+    override fun getName(): @GutterName String {
+        return "Dotenvx variable"
+    }
+
+    override fun getLineMarkerInfo(psiElement: PsiElement): LineMarkerInfo<*>? {
+        if (psiElement is JsonLiteral) {
+            val text = psiElement.text.trim('"')
+            if (text.startsWith("encrypted:")) {
+                return lineMarkerForVariable(psiElement)
+            } else if (text.length == 66 && text.matches(PUBLIC_KEY_REGEX)) {
+                return lineMarkerForPublicKey(psiElement)
+            }
+        }
+        return null
+    }
+}
